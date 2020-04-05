63e736efcd2e1c7bad0505977ac788b08fed2a36
Andrew Jones <andyjones.ed@gmail.com>
2020-04-05 08:28:57 +0000

    Half-way to vectorized tree formation

--- bbfmm/__init__.py
+++ bbfmm/__init__.py
@@ -6,39 +6,23 @@ from itertools import product
 
 EPS = 1e-2
 
-class Grids:
-
-    def __init__(self, depth, dim, tail=(), dtype=float):
-        n = ((2**dim)**depth - 1)/((2**dim) - 1)
-        self._storage = np.zeros((n, *tail), dtype=dtype)
-
-def cartesian_product(xs, D):
-    return np.stack(np.meshgrid(*([xs]*D), indexing='ij'), -1)
-
-def flat_cartesian_product(xs, D):
-    return cartesian_product(xs, D).reshape(-1, D)
-
 def limits(prob):
     points = np.concatenate([prob.sources, prob.targets])
     return np.stack([points.min(0) - EPS, points.max(0) + EPS])
 
-def subdivide(prob, lims):
-    ds = np.arange(prob.sources.shape[-1])
-    center = lims.mean(0)
-    boundaries = np.stack([lims[0], center, lims[1]])
-    options = np.stack(list(product([False, True], repeat=len(ds))))
-    for option in options:
-        masks = aljpy.dotdict(
-            sources=((prob.sources >= center) == option).all(-1),
-            targets=((prob.targets >= center) == option).all(-1))
-        option = option.astype(int)
-        sublims = np.stack([boundaries[option, ds], boundaries[option+1, ds]])
-        yield (tuple(option), masks, sublims)
+def subdivide(xs, lims):
+    lims = np.repeat(lims[None], len(xs), 0)
+    paths = []
 
-def make_grids(prob, cutoff):
-    lims = limits(prob)
+    for _ in range(5):
+        center = lims.mean(1)
+        boundaries = np.stack([lims[:, 0], center, lims[:, 1]], 1)
 
-    while True:
-        finished = (len(prob.sources) <= cutoff) and (len(prob.targets) <= cutoff)
-        if finished:
-            pass
+        fork = (xs >= center).astype(int)
+
+        lims = np.concatenate([
+            np.take_along_axis(boundaries, fork[:, None]+0, axis=1),
+            np.take_along_axis(boundaries, fork[:, None]+1, axis=1)], -2)
+
+        paths.append(fork)
+    paths = np.stack(paths)
