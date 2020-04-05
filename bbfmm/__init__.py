a3ba048de7a464ad4e9e9a5e7fa3dffe5c4aa9d2
Andrew Jones <andyjones.ed@gmail.com>
2020-04-05 20:19:58 +0000

    Swaps to an integer-coded path

--- bbfmm/__init__.py
+++ bbfmm/__init__.py
@@ -10,47 +10,27 @@ def limits(prob):
     points = np.concatenate([prob.sources, prob.targets])
     return np.stack([points.min(0) - EPS, points.max(0) + EPS])
 
-def occupancy(paths):
-    #TODO: numpy has a 32-dim limit hardcoded. So this might cause trouble
-    # if there's a 32-length path, which isn't beyond belief in a non-uniform
-    # input distribution
-    paths = np.stack(paths, 1) if isinstance(paths, list) else paths
-    _, depth, D = paths.shape
-    totals = np.zeros((2,)*(D*depth), dtype=int)
-    np.add.at(totals, tuple(paths.reshape(-1, D*depth).T), 1)
-
-    return totals
-
 def subdivide(xs, lims, cutoff=5):
-    if len(xs) < cutoff:
-        return np.empty((0, len(xs), 2))
+    scaled = (xs - lims[0])/(lims[1] - lims[0])
 
-    lims = np.repeat(lims[None], len(xs), 0)
-    paths = []
+    D = lims.shape[1]
+    paths = np.zeros((len(xs), D), dtype=int)
 
-    while True:
-        center = lims.mean(1)
-        boundaries = np.stack([lims[:, 0], center, lims[:, 1]], 1)
-
-        fork = (xs >= center).astype(int)
-        paths.append(fork)
-
-        lims = np.concatenate([
-            np.take_along_axis(boundaries, fork[:, None]+0, axis=1),
-            np.take_along_axis(boundaries, fork[:, None]+1, axis=1)], -2)
-
-        if occupancy(paths).max() <= cutoff:
+    #TODO: You can probably get very smart about this and just cast the xs
+    # to ints and look at their binary representation.
+    for depth in range(32):
+        occupancy = np.zeros((2**depth,)*D)
+        np.add.at(occupancy, tuple(paths.T), 1)
+        if occupancy.max() <= cutoff:
             break
 
-    return np.stack(paths, 1)
+        boundaries = (1/2 + np.arange(2**depth))/2**depth
+        paths = 2*paths + (scaled >= boundaries[paths]).astype(int)
+    else:
+        raise ValueError('Paths seem very long')
 
-def sublimits(lims, depth):
-    for d in range(depth):
-        center = lims.mean(-2)
-        lower = np.stack([lims[..., 0, :], center], -2)
-        upper = np.stack([center, lims[..., 1, :]], -2)
-        lims = np.stack([lower, upper], -3)
-    return lims
+    return paths, depth
+
 
 
 def run():
