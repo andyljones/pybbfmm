ab252d7d5174ea4814ccf8a79b6027d95525bc27
Andrew Jones <andyjones.ed@gmail.com>
2020-04-10 13:05:39 +0000

    Gets as far as implementing `weights` in pytorch

---
+++ pybbfmm/pytorch/chebyshev.py
@@ -0,0 +1,68 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+def cartesian_product(xs, D):
+    xs = torch.tensor(xs)
+    return torch.stack(torch.meshgrid(*((xs,)*D)), -1)
+
+def flat_cartesian_product(xs, D):
+    return cartesian_product(xs, D).reshape(-1, D)
+
+class Chebyshev:
+    
+    def __init__(self, N, D):
+        self.N = N
+        self.D = D
+        self._nodes = None
+
+    @property
+    def nodes(self):
+        if self._nodes is None:
+            ms = torch.arange(self.N)
+            onedim = torch.cos((ms+1/2)*np.pi/self.N)
+            self._nodes = flat_cartesian_product(onedim, self.D)
+        return self._nodes
+
+    def similarity(self, a, b):
+        """
+        Args:
+            a: ms + (d,)
+            b: ns + (d,)
+        
+        Returns:
+            ms + ns
+        """
+        assert ((-1 <= a) & (a <= +1)).all()
+        assert ((-1 <= b) & (b <= +1)).all()
+
+        da = a.shape[:-1] + (1,)*(b.ndim-1) + (self.D, 1)
+        db = (1,)*(a.ndim-1) + b.shape[:-1] + (self.D, 1)
+
+        theta_a = torch.acos(a).reshape(da)
+        theta_b = torch.acos(b).reshape(db)
+
+        ks = torch.arange(1, self.N)[None, None, None, :]
+        terms = torch.cos(ks*theta_a)*torch.cos(ks*theta_b)
+        return (1/self.N + 2/self.N*terms.sum(-1)).prod(-1)
+
+    def interpolate(self, x, v):
+        return (self.similarity(x, self.nodes)*v).sum(-1)
+
+    def anterpolate(self, x, v):
+        return (self.similarity(self.nodes, x)*v).sum(-1)
+
+def test_similarity():
+
+    cheb = Chebyshev(4, 1)
+
+    def g(x):
+        return (x**5).sum(-1)
+
+    ns = cheb.nodes
+    xs = torch.linspace(-1, +1, 101)[:, None]
+    ghat = (cheb.similarity(xs, ns)*g(ns)).sum(-1)
+
+    plt.plot(xs[:, 0], g(xs))
+    plt.plot(xs[:, 0], ghat)
+
---
+++ pybbfmm/pytorch/test.py
@@ -0,0 +1,17 @@
+import torch
+import aljpy
+import numpy as np
+
+def quad_kernel(a, b):
+    return 1/((a - b)**2).sum(-1)
+
+def random_problem(S=3, T=5, D=2):
+    return (aljpy.dotdict(
+        sources=np.random.uniform(-1., +1., (S, D)),
+        charges=np.random.uniform(.1, 1., (S,)),
+        targets=np.random.uniform(-1., +1., (T, D)))
+            .map(lambda t: torch.tensor(t, dtype=torch.float)))
+
+def solve(prob, kernel=quad_kernel):
+    k = kernel(prob.targets[:, None], prob.sources[None, :])
+    return (k*prob.charges).sum(-1)
