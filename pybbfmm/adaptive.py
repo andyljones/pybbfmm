0e0f9983a4c8b8c23121ec14792c34d0d38b8dd6
Andrew Jones <andyjones.ed@gmail.com>
2020-04-15 13:48:43 +0000

    Implements weights

--- pybbfmm/adaptive.py
+++ pybbfmm/adaptive.py
@@ -1,10 +1,7 @@
 import aljpy
 from aljpy import arrdict
 from . import test, chebyshev, interactions
-import numpy as np
 import torch
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 
 KERNEL = test.quad_kernel
 EPS = 1e-2
@@ -80,7 +77,39 @@ def orthant_tree(scaled, cutoff=5):
 
     tree['id'] = torch.arange(len(tree.parents), device=points.device)
 
+    indices = arrdict.arrdict(
+        sources=indices[:len(scaled.sources)],
+        targets=indices[:len(scaled.targets)])
+
     return tree, indices
+
+def accumulate(indices, vals, length):
+    totals = vals.new_zeros((length,) + vals.shape[1:])
+    totals.index_add_(0, indices, vals)
+    return totals
+
+def uplift_coeffs(cheb):
+    shifts = torch.tensor([-.5, +.5], device=cheb.device)
+    shifts = chebyshev.cartesian_product(shifts, cheb.D)
+    children = shifts[..., None, :] + cheb.nodes/2
+    return cheb.similarity(cheb.nodes, children)
+
+def weights(scaled, cheb, tree, indices):
+    loc = 2**tree.depths[indices.sources, None]*(scaled.sources - tree.centers[indices.sources])
+    S = cheb.similarity(loc, cheb.nodes)
+    W = accumulate(indices.sources, S, len(tree.id))
+
+    coeffs = uplift_coeffs(cheb)
+    dot_dims = (list(range(1, cheb.D+2)), list(range(1, cheb.D+2)))
+
+    parents = tree.parents[indices.sources]
+    while (parents >= 0).any():
+        parents = parents[parents >= 0]
+        W[parents] = torch.tensordot(W[tree.children[parents]], coeffs, dot_dims)
+        parents = tree.parents[parents] 
+
+    return W
+
 
 def run():
     torch.random.manual_seed(1)
@@ -89,10 +118,12 @@ def run():
         charges=torch.tensor([1., 1.]),
         targets=torch.empty((0, 2)))
 
-    prob = test.random_problem(T=500)
+    prob = test.random_problem(S=100, T=100)
 
     scaled = scale(prob)
+    cheb = chebyshev.Chebyshev(4, scaled.sources.shape[1], device='cuda')
 
     tree, indices = orthant_tree(scaled)
-
     lists = interactions.lists(tree)
+
+    ws = weights(scaled, cheb, tree, indices)
--- pybbfmm/plotting.py
+++ pybbfmm/plotting.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from . import interactions
@@ -37,8 +38,8 @@ def plot_problem(prob, q=.01, ax=None):
 
     return ax
 
-def plot_lists(tree, lists, b):
-    b = int(tree.terminal.nonzero().squeeze(-1)[0])
+def plot_lists(tree, lists, b=None):
+    b = int(tree.terminal.nonzero().squeeze(-1)[0]) if b is None else b
     color = {'u': 'C0', 'v': 'C1', 'w': 'C2', 'x': 'C3', 'y': 'C4'}
     key = {k: mpl.patches.Rectangle((0., 0.), 1, 1, color=c, alpha=.25) for k, c in color.items()}
     color = {color[k]: l[l[:, 0] == b, 1] for k, l in lists.items()}
