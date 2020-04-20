79e8b8026c98606cf85676d980fc789276edb585
Andrew Jones <andyjones.ed@gmail.com>
2020-04-20 15:43:56 +0000

    Makes the GPU optional

--- pybbfmm/test.py
+++ pybbfmm/test.py
@@ -6,11 +6,11 @@ import numpy as np
 def quad_kernel(a, b):
     return 1/((a - b)**2).sum(-1)
 
-def random_problem(S=3, T=5, D=2):
+def random_problem(S=3, T=5, D=2, device='cuda'):
     prob = arrdict.arrdict(
         sources=np.random.uniform(-1., +1., (S, D)),
         charges=np.random.uniform(.1, 1., (S,)),
-        targets=np.random.uniform(-1., +1., (T, D))).map(torch.tensor).float().cuda()
+        targets=np.random.uniform(-1., +1., (T, D))).map(torch.tensor).float().to(device)
     prob['kernel'] = quad_kernel
     return prob
 
