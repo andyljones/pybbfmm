269ee60bb0b860f8ff090f3b9b08381957976eef
Andrew Jones <andyjones.ed@gmail.com>
2020-04-20 16:18:10 +0000

    Adds av dep

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='pybbfmm',
       python_requires='>=3.6',
       install_requires=[
           'torch>=1.4', 'aljpy>=0.4', 'numpy>=1.18'],
-      extra_requires={'demo': [
-          'matplotlib>=3.2', 'scipy>=1.4', 'ipython>=7.5', 'tqdm>=4.42', 'requests>=2.22']}) 
+      extras_require={'demo': [
+          # av~=6.2 as later versions require ffmpeg 4, which isn't in most distro's repos.
+          'matplotlib>=3', 'scipy>=1.4', 'ipython>=5', 'tqdm>=4', 'requests>=2', 'av~=6.2']}) 
 
