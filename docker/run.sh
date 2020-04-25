6da537cc64818b360ab32af9e540394bec0b79bf
Andrew Jones <andyjones.ed@gmail.com>
2020-04-25 12:01:39 +0100

    Adds a Dockerfile

---
+++ docker/run.sh
@@ -0,0 +1,10 @@
+echo "Launching SSH"
+service ssh start
+
+echo "Launching Jupyter"
+cd code
+mkdir output 
+jupyter notebook --notebook-dir . --no-browser --port=5000 --ip=0.0.0.0 --allow-root --NotebookApp.token="" --NotebookApp.password="" >output/jupyter.log 2>&1 &
+
+echo "Launching $@"
+"$@"
