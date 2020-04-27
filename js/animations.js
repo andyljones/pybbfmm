c02c06615848795da457c6f10e32b4e53607ea0f
Andrew Jones <andyjones.ed@gmail.com>
2020-04-27 13:12:13 +0000

    Moved the blogpost stuff back to Notable

--- js/animations.js
+++
@@ -1,9 +0,0 @@
-import d3 from 'https://d3js.org/d3.v5.min.js';
-
-function random_problem(S, T, W) {
-    return {
-        sources: d3.range(0, S).map(() => d3.randomUniform(.5, W-.5)),
-        targets: d3.range(0, W, W/T),
-        kernel: (a, b) => 1/(1 + ((a-b)*2)**2)
-    }
-}
