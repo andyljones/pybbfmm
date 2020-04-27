dd68fe9c98c69977193df06517887c6dfd18c144
Andrew Jones <andyjones.ed@gmail.com>
2020-04-27 12:38:13 +0000

    Checkpoint

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,9 @@
-function test () {
-    console.log('hello world');
+import d3 from 'https://d3js.org/d3.v5.min.js';
+
+function random_problem(S, T, W) {
+    return {
+        sources: d3.range(0, S).map(() => d3.randomUniform(.5, W-.5)),
+        targets: d3.range(0, W, W/T),
+        kernel: (a, b) => 1/(1 + ((a-b)*2)**2)
+    }
 }
