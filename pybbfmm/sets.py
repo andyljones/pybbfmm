dc902549ac71d6e27c3aae25799896d449b00e44
Andrew Jones <andyjones.ed@gmail.com>
2020-07-24 14:18:07 +0000

    Adds sets docs

--- pybbfmm/sets.py
+++ pybbfmm/sets.py
@@ -1,8 +1,12 @@
 import torch
 
 def cartesian_product(xs, D):
+    """Returns all D-tuples of ``xs`` as a (n_xs, n_xs, ..., \\*tail_dims)-tensor.
+    """
     xs = torch.as_tensor(xs)
     return torch.stack(torch.meshgrid(*((xs,)*D)), -1)
 
 def flat_cartesian_product(xs, D):
+    """Returns all D-tuples of ``xs`` as a (n_xs :sup:`D`, \\*tail_dims)-tensor.
+    """
     return cartesian_product(xs, D).reshape(-1, D)
